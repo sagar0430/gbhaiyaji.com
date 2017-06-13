9ef176a481fd5084427bb5bc2e6ce64f7ff24087
AshishkrGoyal <15103140ashish@gmail.com>
2017-06-13 13:25:39 +0530

    scrolling animation added

---
+++ scroll_nav.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Ashish Goyal on 6/13/2017.
+ */
+$(document).on("scroll", function(){
+    if
+    ($(document).scrollTop() > 50){
+        $(".navbar").addClass("shrink");
+    }
+    else
+    {
+        $(".navbar").removeClass("shrink");
+    }
+});
